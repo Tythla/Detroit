2037bcf2b11245a1df3eeb71f9eb1925cb4e16ba
tythla <geeksoap@outlook.com>
2026-03-15 22:35:38 -0700

    add eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,7 @@
 import js from '@eslint/js'
+import importPlugin from 'eslint-plugin-import'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import react from 'eslint-plugin-react'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
@@ -12,9 +15,60 @@ export default defineConfig([
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+      react.configs.flat.recommended,
+      react.configs.flat['jsx-runtime'],
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      jsxA11y.flatConfigs.recommended,
+      importPlugin.flatConfigs.recommended,
+      importPlugin.flatConfigs.typescript,
     ],
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      'import/resolver': {
+        typescript: true,
+        node: true,
+      },
+    },
+    rules: {
+      '@typescript-eslint/consistent-type-imports': [
+        'warn',
+        { prefer: 'type-imports' },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      curly: ['error', 'all'],
+      eqeqeq: ['error', 'smart'],
+      'import/no-unresolved': 'off',
+      'import/order': [
+        'warn',
+        {
+          alphabetize: { caseInsensitive: true, order: 'asc' },
+          'newlines-between': 'always',
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling', 'index'],
+            'object',
+            'type',
+          ],
+        },
+      ],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-debugger': 'warn',
+      'no-multiple-empty-lines': ['warn', { max: 1, maxBOF: 0, maxEOF: 0 }],
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'react/jsx-boolean-value': ['error', 'never'],
+      'react/jsx-no-useless-fragment': 'warn',
+      'react/prop-types': 'off',
+      'react/self-closing-comp': 'warn',
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
